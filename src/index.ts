4d52c56069641c2307c699f295f18c72220c5227
i-m-afk <rishavkumar208@gmail.com>
2023-09-02 02:46:53 +0530

    for some reason chrome doesn't like port 6666

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import app from './server'
 import * as dotenv from 'dotenv'
 dotenv.config()
 
-const PORT = process.env.PORT || 6666
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
     console.log("server is running on port " + PORT)
